773ce6f8bdb05ac62aa25a3bf170a93d6d85c2e3
prem-choithani23 <premchoithani4@gmail.com>
2026-03-21 23:40:39 +0530

    feat: add hex_dump utility to inspect raw binary file

--- main.c
+++ main.c
@@ -20,6 +20,7 @@ void read_record(FILE * file , int index , Student *out)
 
 }
 
+
 void update_record(FILE * file , int index , Student *updated)
 {
     // move the file ptr to correct indexed row using offset
@@ -41,12 +42,14 @@ void insert_rows(FILE * file)
     // temp array for storing names
     char new_name[50];
 
+
     // number of items to write
     int num_items = 100;
 
     for (int i=0;i<num_items;i++)
     {
         // all students having name of form 'Student_id'
+
         char name [11] = "Student_";
         char id[3];
 
@@ -63,12 +66,15 @@ void insert_rows(FILE * file)
         int age = 15 + rand()%30;
 
         Student student;
+        memset(&student , 0 , sizeof(Student));
 
         // set id
         student.id = i;
 
-        // set name ( using pcpy) and not 'new_name = name'
+        // set name ( using cpy) and not 'new_name = name'
         strcpy(student.name ,new_name);
+
+
 
         // set age
         student.age = age;
@@ -97,52 +103,83 @@ void print_db(FILE * file ,int num_records)
         print_student(s);
     }
 }
-int main(void)
+
+void hex_dump(const char * filename ,int num_records)
 {
-    srand(time(NULL));
-    FILE * file = fopen("students.db" ,"rb+");
+    FILE * file = fopen(filename , "rb");
 
-
-    if (file == NULL)
+    for (int i=0;i<num_records;i++)
     {
-        printf("File opening failed");
-        return 0;
+        int stream = fseek(file, i*sizeof(Student) ,SEEK_SET);
+
+        for (int byte = 0;byte<sizeof(Student);byte++)
+        {
+
+
+            unsigned char value;
+            fread(&value , 1  , 1 , file);
+
+            // zero pad printing (hex value in byte)
+            printf("%02x " , value);
+
+        }
+        printf("\n");
     }
 
-    // write 100 rows
-    insert_rows(file);
-
-
-    // search key
-    int search_id = 13;
-    Student s;
-    // read record into s
-    read_record(file , search_id , &s);
-
-    // print s
-    printf("Before Update : ");
-    print_student(s);
-
-
-    // modified student
-    Student updated = {search_id , "Ramesh",37};
-
-    // replace contents of row 'index' by contents of 'updated'
-    update_record(file , search_id , &updated);
-
-
-    printf("After Update : ");
-
-    // read record into s
-    read_record(file , search_id , &s);
-    // print s
-    print_student(s);
-
-    // print all rows (sample)
-    // print_db(file , 100);
-
-    // close file connection
     fclose(file);
-
-    return 0;
 }
+
+int main(int argc, char* argv[])
+{
+    hex_dump("students.db" , 100);
+}
+
+// int main(void)
+// {
+//     srand(time(NULL));
+//     FILE * file = fopen("students.db" ,"rb+");
+//
+//
+//     if (file == NULL)
+//     {
+//         printf("File opening failed");
+//         return 0;
+//     }
+//
+//     // write 100 rows
+//     insert_rows(file);
+//
+//
+//     // search key
+//     int search_id = 13;
+//     Student s;
+//     // read record into s
+//     read_record(file , search_id , &s);
+//
+//     // print s
+//     printf("Before Update : ");
+//     print_student(s);
+//
+//
+//     // modified student
+//     Student updated = {search_id , "Ramesh",37};
+//
+//     // replace contents of row 'index' by contents of 'updated'
+//     update_record(file , search_id , &updated);
+//
+//
+//     printf("After Update : ");
+//
+//     // read record into s
+//     read_record(file , search_id , &s);
+//     // print s
+//     print_student(s);
+//
+//     // print all rows (sample)
+//     // print_db(file , 100);
+//
+//     // close file connection
+//     fclose(file);
+//
+//     return 0;
+// }
