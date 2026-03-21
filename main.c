c9e0de63c6d4a96a055e0535a3a27165d0e2b28a
prem-choithani23 <premchoithani4@gmail.com>
2026-03-21 12:46:14 +0530

    refactor: add helper comments

--- main.c
+++ main.c
@@ -12,15 +12,20 @@ void int_to_string(int n , char * buffer)
 
 void read_record(FILE * file , int index , Student *out)
 {
+    // move the file ptr to correct indexed row using offset
     int stream = fseek(file, (index) * sizeof(Student) , SEEK_SET);
 
+    // read the row in location pointed by 'out'
     fread(out , sizeof(Student) , 1 , file);
 
 }
 
 void update_record(FILE * file , int index , Student *updated)
 {
+    // move the file ptr to correct indexed row using offset
     int stream = fseek(file , index*sizeof(Student) ,SEEK_SET );
+
+    // write the row with contents present in location pointed by 'updated'
 
     fwrite(updated , sizeof(Student) , 1 ,file);
 
@@ -30,32 +35,47 @@ void update_record(FILE * file , int index , Student *updated)
 
 void insert_rows(FILE * file)
 {
+    // define size of item
     size_t size_of_item = sizeof(Student);
+
+    // temp array for storing names
     char new_name[50];
 
+    // number of items to write
     int num_items = 100;
 
     for (int i=0;i<num_items;i++)
     {
+        // all students having name of form 'Student_id'
         char name [11] = "Student_";
         char id[3];
 
+        // converting id to string
         int_to_string(i , id);
 
+        // append the converted int to name
         strcat(name , id);
 
-
+        // as arrays cannot be changes by simply changie the reference , we do cpy
         strcpy(new_name, name);
 
+        // define a random age
         int age = 15 + rand()%30;
 
         Student student;
 
+        // set id
         student.id = i;
+
+        // set name ( using pcpy) and not 'new_name = name'
         strcpy(student.name ,new_name);
+
+        // set age
         student.age = age;
 
+        // write 'size_of_item' bytes with the contents pointed by &s , 1 time . at location pointed by file
         int write = fwrite( &student,size_of_item , 1, file);
+
         if (write == 0)
         {
             printf("Write failed...");
@@ -93,26 +113,35 @@ int main(void)
     insert_rows(file);
 
 
+    // search key
     int search_id = 13;
     Student s;
+    // read record into s
     read_record(file , search_id , &s);
 
-
-
+    // print s
     printf("Before Update : ");
     print_student(s);
 
+
+    // modified student
     Student updated = {search_id , "Ramesh",37};
 
+    // replace contents of row 'index' by contents of 'updated'
     update_record(file , search_id , &updated);
 
+
     printf("After Update : ");
+
+    // read record into s
     read_record(file , search_id , &s);
+    // print s
     print_student(s);
 
+    // print all rows (sample)
+    // print_db(file , 100);
 
-    print_db(file , 100);
-
+    // close file connection
     fclose(file);
 
     return 0;
