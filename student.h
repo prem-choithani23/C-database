3944a347892582e16f25f6c41afa25983e332ba8
prem-choithani23 <premchoithani4@gmail.com>
2026-03-21 09:49:01 +0530

    feat: add Student struct with include guard

---
+++ student.h
@@ -0,0 +1,16 @@
+//
+// Created by prem on 3/21/26.
+//
+
+#ifndef CDB_STUDENT_H
+#define CDB_STUDENT_H
+
+typedef struct student
+{
+    int id;
+    char name[50];
+    int age;
+}Student;
+
+#endif //CDB_STUDENT_H
+
