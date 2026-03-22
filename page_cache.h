a8fdbee2562a7f97c569ea908cb312227d2cebb0
prem-choithani23 <premchoithani4@gmail.com>
2026-03-22 09:55:54 +0530

    feat: update struct of PageCache include array of PageEntry instead of SoftStudent & a clock

--- page_cache.h
+++ page_cache.h
@@ -5,10 +5,19 @@
 #ifndef CDB_PAGE_CACHE_H
 #define CDB_PAGE_CACHE_H
 #include "modified_student.h"
+typedef struct entry PageEntry;
+
 typedef struct
 {
-    SoftStudent *pages;
+    PageEntry *pages;
     int capacity;
     int count;
+    int clock;
 }PageCache;
+
+typedef struct entry
+{
+    SoftStudent student;
+    int last_used;
+}PageEntry;
 #endif //CDB_PAGE_CACHE_H
